df5a7085524fc0bb80d60692e045e3005f76ecd5
whooie <wchooie@gmail.com>
2025-04-25 17:05:03 -0500

    remove openblas dependency/feature

--- tensor-net/lib/lib.rs
+++ tensor-net/lib/lib.rs
@@ -58,29 +58,8 @@ pub trait ComplexFloatExt: ComplexFloat /*+ std::fmt::Debug*/
 
     /// Convert a polar representation into a complex number.
     fn from_polar(r: Self::Real, theta: Self::Real) -> Self;
-
-    #[cfg(feature = "openblas")]
-    /// Construct from real and imaginary components encoded in `f64`s.
-    fn from_f64(re: f64, im: f64) -> Self;
-
-    #[cfg(feature = "openblas")]
-    /// Return the real component encoded in an `f64`.
-    fn real_f64(self) -> f64;
-
-    #[cfg(feature = "openblas")]
-    /// Return the imaginary component encoded in an `f64`.
-    fn imag_f64(self) -> f64;
-
-    #[cfg(feature = "openblas")]
-    /// Convert a real value to a `f64`.
-    fn real_into_f64(re: Self::Real) -> f64;
-
-    #[cfg(feature = "openblas")]
-    /// Convert a `f64` to `Self::Real`.
-    fn real_from_f64(re: f64) -> Self::Real;
 }
 
-#[cfg(not(feature = "openblas"))]
 impl<T> ComplexFloatExt for Complex<T>
 where
     Complex<T>: ComplexFloat<Real = T>,
@@ -109,58 +88,6 @@ where
     fn from_polar(r: Self::Real, theta: Self::Real) -> Self {
         Complex::from_polar(r, theta)
     }
-}
-
-#[cfg(feature = "openblas")]
-impl ComplexFloatExt for Complex<f32> {
-    fn i() -> Self { Self::i() }
-
-    fn from_re(x: f32) -> Self { Self { re: x, im: f32::zero() } }
-
-    fn from_components(re: f32, im: f32) -> Self { Self { re, im } }
-
-    fn cis(angle: f32) -> Self { Self::cis(angle) }
-
-    fn to_polar(self) -> (f32, f32) { self.to_polar() }
-
-    fn from_polar(r: f32, theta: f32) -> Self { Self::from_polar(r, theta) }
-
-    fn from_f64(re: f64, im: f64) -> Self {
-        Self { re: re as f32, im: im as f32 }
-    }
-
-    fn real_f64(self) -> f64 { self.re.into() }
-
-    fn imag_f64(self) -> f64 { self.im.into() }
-
-    fn real_into_f64(re: f32) -> f64 { re.into() }
-
-    fn real_from_f64(re: f64) -> f32 { re as f32 }
-}
-
-#[cfg(feature = "openblas")]
-impl ComplexFloatExt for Complex<f64> {
-    fn i() -> Self { Self::i() }
-
-    fn from_re(x: f64) -> Self { Self { re: x, im: f64::zero() } }
-
-    fn from_components(re: f64, im: f64) -> Self { Self { re, im } }
-
-    fn cis(angle: f64) -> Self { Self::cis(angle) }
-
-    fn to_polar(self) -> (f64, f64) { self.to_polar() }
-
-    fn from_polar(r: f64, theta: f64) -> Self { Self::from_polar(r, theta) }
-
-    fn from_f64(re: f64, im: f64) -> Self { Self { re, im } }
-
-    fn real_f64(self) -> f64 { self.re }
-
-    fn imag_f64(self) -> f64 { self.im }
-
-    fn real_into_f64(re: f64) -> f64 { re }
-
-    fn real_from_f64(re: f64) -> f64 { re }
 }
 
 // NOTE: Dependence on ComplexFloat seems to be mostly redundant via
