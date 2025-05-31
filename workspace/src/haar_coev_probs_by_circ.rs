5f1e8aeef1652d05410df15b6b1f6964f7fa3a60
Will Huie <whuie2@illinois.edu>
2025-05-31 16:16:58 -0500

    adapt for use on the cluster

--- workspace/src/haar_coev_probs_by_circ.rs
+++ workspace/src/haar_coev_probs_by_circ.rs
@@ -14,7 +14,7 @@ use tensor_net::{
 };
 use whooie::write_npz;
 
-const N: usize = 16;
+const N: usize = 19;
 const T: usize = 10 * N;
 const CIRCS: usize = 30;
 const P_MEAS: &[f64] = &[
@@ -29,7 +29,7 @@ const BONDS: &[Option<usize>] = &[
     None,
     Some(4),  Some(8),  Some(12), Some(16), Some(20),
     Some(24), Some(28), Some(32), Some(36), Some(40),
-    Some(44), Some(48),
+    Some(44), Some(48), Some(56), Some(64), Some(72),
 ];
 const CIRCUIT_SEED: u64 = 10546;
 
@@ -135,119 +135,116 @@ fn main() {
     //     .parse::<usize>().unwrap();
     let task_id = Alphanumeric.sample_string(&mut thread_rng(), 10);
 
-    for i in 0..CIRCS {
-        if !circuit_dir.join(Instance::Unis.fmt(i, None, false)).is_file() {
-            panic!("missing unitaries file for circuit {}", i);
-        }
-        if !circuit_dir.join(Instance::Meas.fmt(i, None, false)).is_file() {
-            panic!("missing measurement locations file for circuit {}", i);
-        }
+    let circ: usize =
+        std::env::args()
+        .nth(1)
+        .expect("missing circuit number")
+        .parse::<usize>()
+        .expect("invalid circuit number");
+    if !circuit_dir.join(Instance::Unis.fmt(circ, None, false)).is_file() {
+        panic!("missing unitaries file for circuit {}", circ);
     }
+    if !circuit_dir.join(Instance::Meas.fmt(circ, None, false)).is_file() {
+        panic!("missing measurement locations file for circuit {}", circ);
+    }
+    println!("running circuit {}", circ);
 
     let n_p = P_MEAS.len();
     let n_b = BONDS.iter().flatten().count() + 1;
 
     // output text formatting
-    let w_c: usize = (CIRCS as f64).log10().floor() as usize + 1;
     let w_p: usize = (P_MEAS.len() as f64).log10().floor() as usize + 1;
     let w_run: usize = (RUNS as f64).log10().floor() as usize + 1;
 
-    // eprint!(" {:w_c$} / {:w_c$} ", 0, CIRCS);
-    for i in 0..CIRCS {
-        // eprint!("\x1b[{}D{:w_c$} / {:w_c$} ",
-        //     2 * w_c + 4, i + 1, CIRCS);
+    // traj_data :: { p, run, t, x }
+    let mut traj_data: nd::Array4<i8> =
+        nd::Array::zeros((n_p, RUNS, T, N));
+    // prob_data :: { p, run, chi, t, x }
+    let mut prob_data: nd::Array5<f64> =
+        nd::Array::from_elem((n_p, RUNS, n_b, T, N), -1.0);
 
-        // traj_data :: { p, run, t, x }
-        let mut traj_data: nd::Array4<i8> =
-            nd::Array::zeros((n_p, RUNS, T, N));
-        // prob_data :: { p, run, chi, t, x }
-        let mut prob_data: nd::Array5<f64> =
-            nd::Array::from_elem((n_p, RUNS, n_b, T, N), -1.0);
+    let unis: Elements<UniSeq> = Elements::load(
+        circuit_dir.join(Instance::Unis.fmt(circ, None, false))).unwrap();
+    let meas: Elements<Elements<MeasSeq>> = Elements::load(
+        circuit_dir.join(Instance::Meas.fmt(circ, None, false))).unwrap();
 
-        let unis: Elements<UniSeq> = Elements::load(
-            circuit_dir.join(Instance::Unis.fmt(i, None, false))).unwrap();
-        let meas: Elements<Elements<MeasSeq>> = Elements::load(
-            circuit_dir.join(Instance::Meas.fmt(i, None, false))).unwrap();
+    // eprint!(" {:w_p$} / {:w_p$} ", 0, n_p);
+    let p_iter =
+        traj_data.outer_iter_mut()
+        .zip(prob_data.outer_iter_mut())
+        .zip(meas.0.iter())
+        .enumerate();
+    for (j, ((mut traj_p, mut prob_p), meas_p)) in p_iter {
+        // eprint!("\x1b[{}D{:w_p$} / {:w_p$} ",
+        //     2 * w_p + 4, j + 1, n_p);
+        eprintln!("{:w_p$} / {:w_p$} ", j + 1, n_p);
 
-        // eprint!(" {:w_p$} / {:w_p$} ", 0, n_p);
-        let p_iter =
-            traj_data.outer_iter_mut()
-            .zip(prob_data.outer_iter_mut())
-            .zip(meas.0.iter())
-            .enumerate();
-        for (j, ((mut traj_p, mut prob_p), meas_p)) in p_iter {
-            // eprint!("\x1b[{}D{:w_p$} / {:w_p$} ",
-            //     2 * w_p + 4, j + 1, n_p);
-            eprint!("{:w_c$} / {:w_c$} ", i + 1, CIRCS);
-            eprintln!("{:w_p$} / {:w_p$} ", j + 1, n_p);
-
-            let run = AtomicUsize::new(0);
-            // eprint!(" {:w_run$} / {:w_run$} ", run.get_mut(), RUNS);
-            nd::Zip::from(traj_p.outer_iter_mut())
-                .and(prob_p.outer_iter_mut())
-                .par_for_each(|mut traj_pr, mut prob_pr| {
-                    let Trajectory { traj, probs } =
-                        sample_trajectory((unis.as_ref(), meas_p.as_ref()));
-                    traj.into_iter()
-                    .zip(traj_pr.outer_iter_mut())
-                    .for_each(|(meas_layer, mut meas_rec)| {
-                        meas_layer.into_iter()
-                        .for_each(|m| {
-                            match m {
-                                Meas::Rand(_) => { },
-                                Meas::Proj(k, out) => {
-                                    meas_rec[k] =
-                                        match out {
-                                            Outcome::Zero => -1,
-                                            Outcome::One => 1,
-                                        };
-                                },
-                            }
-                        });
+        let run = AtomicUsize::new(0);
+        // eprint!(" {:w_run$} / {:w_run$} ", run.get_mut(), RUNS);
+        nd::Zip::from(traj_p.outer_iter_mut())
+            .and(prob_p.outer_iter_mut())
+            .par_for_each(|mut traj_pr, mut prob_pr| {
+                let Trajectory { traj, probs } =
+                    sample_trajectory((unis.as_ref(), meas_p.as_ref()));
+                traj.into_iter()
+                .zip(traj_pr.outer_iter_mut())
+                .for_each(|(meas_layer, mut meas_rec)| {
+                    meas_layer.into_iter()
+                    .for_each(|m| {
+                        match m {
+                            Meas::Rand(_) => { },
+                            Meas::Proj(k, out) => {
+                                meas_rec[k] =
+                                    match out {
+                                        Outcome::Zero => -1,
+                                        Outcome::One => 1,
+                                    };
+                            },
+                        }
                     });
-                    probs.into_iter()
-                    .zip(prob_pr.outer_iter_mut())
-                    .for_each(|(prob_data_prx, mut prob_rec_prx)| {
-                        prob_data_prx.into_iter()
-                        .zip(prob_rec_prx.outer_iter_mut())
-                        .for_each(|(prob_layer, mut prob_rec)| {
-                            prob_layer.into_iter()
-                            .for_each(|(k, p)| { prob_rec[k] = p; });
-                        });
-                    });
-                    let prev_run = run.fetch_add(1, Ordering::SeqCst);
-                    // eprint!("\x1b[{}D{:w_run$} / {:w_run$} ",
-                    //     2 * w_run + 4, prev_run + 1, RUNS);
-                    eprintln!("  {:w_run$} / {:w_run$} ", prev_run + 1, RUNS);
                 });
-            // eprint!("\x1b[{}D", 2 * w_run + 5);
-        }
-        // eprint!("\x1b[{}D", 2 * w_p + 5);
-        let fname = Instance::Output.fmt(i, Some(&task_id), true);
-        write_npz!(
-            outdir.join(fname),
-            arrays: {
-                "size" => &nd::array![N as i32],
-                "depth" => &nd::array![T as i32],
-                "circ" => &nd::array![i as i32],
-                "circs" => &nd::array![CIRCS as i32],
-                "runs" => &nd::array![RUNS as i32],
-                "p_meas" =>
-                    &P_MEAS.iter().copied()
-                    .collect::<nd::Array1<f64>>(),
-                "chi" =>
-                    &[0_i32].into_iter()
-                    .chain(
-                        BONDS.iter().copied()
-                        .filter_map(|mb_chi| mb_chi.map(|x| x as i32))
-                    )
-                    .collect::<nd::Array1<i32>>(),
-                "traj" => &traj_data,
-                "prob" => &prob_data,
-                "seed" => &nd::array![CIRCUIT_SEED as i32],
-            }
-        );
+                probs.into_iter()
+                .zip(prob_pr.outer_iter_mut())
+                .for_each(|(prob_data_prx, mut prob_rec_prx)| {
+                    prob_data_prx.into_iter()
+                    .zip(prob_rec_prx.outer_iter_mut())
+                    .for_each(|(prob_layer, mut prob_rec)| {
+                        prob_layer.into_iter()
+                        .for_each(|(k, p)| { prob_rec[k] = p; });
+                    });
+                });
+                let prev_run = run.fetch_add(1, Ordering::SeqCst);
+                // eprint!("\x1b[{}D{:w_run$} / {:w_run$} ",
+                //     2 * w_run + 4, prev_run + 1, RUNS);
+                eprintln!("  {:w_run$} / {:w_run$} ", prev_run + 1, RUNS);
+            });
+        // eprint!("\x1b[{}D", 2 * w_run + 5);
     }
+    // eprint!("\x1b[{}D", 2 * w_p + 5);
+    let fname = Instance::Output.fmt(circ, Some(&task_id), true);
+    write_npz!(
+        outdir.join(fname),
+        arrays: {
+            "size" => &nd::array![N as i32],
+            "depth" => &nd::array![T as i32],
+            "circ" => &nd::array![circ as i32],
+            "circs" => &nd::array![CIRCS as i32],
+            "runs" => &nd::array![RUNS as i32],
+            "p_meas" =>
+                &P_MEAS.iter().copied()
+                .collect::<nd::Array1<f64>>(),
+            "chi" =>
+                &[0_i32].into_iter()
+                .chain(
+                    BONDS.iter().copied()
+                    .filter_map(|mb_chi| mb_chi.map(|x| x as i32))
+                )
+                .collect::<nd::Array1<i32>>(),
+            "traj" => &traj_data,
+            "prob" => &prob_data,
+            "seed" => &nd::array![CIRCUIT_SEED as i32],
+        }
+    );
     eprintln!();
 }
 
